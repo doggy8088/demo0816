35aa891d9c5089ba2b85ce3789f1469e0a8255a0
Will <doggy.huang@gmail.com>
2016-08-16 11:23:15 +0800

    05 練習 Angular 2 資料繫結方法：內嵌繫結 ( interpolation )

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  title = 'The Will Will Web !';
+  titleurl = 'http://blog.miniasp.com/';
+
   constructor() { }
 
   ngOnInit() {
